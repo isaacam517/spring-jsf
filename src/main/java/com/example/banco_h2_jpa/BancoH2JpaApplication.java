986e1e2ebfc76f929de0152b300780001a2f17cc
Isaac Martiniano <isaacam517@gmail.com>
2024-11-06 00:34:35 -0300

    primeiro commit

---
+++ src/main/java/com/example/banco_h2_jpa/BancoH2JpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.banco_h2_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoH2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoH2JpaApplication.class, args);
+	}
+
+}
